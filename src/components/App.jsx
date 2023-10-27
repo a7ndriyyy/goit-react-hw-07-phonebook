f1fce30cc6a36cd6253a17a524b4a9398de06ae9
a7ndriyyy <andrii.nastych@gmail.com>
2023-10-27 13:29:38 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
+import { fetchContacts, addContact } from 'redux/Contacts/operations';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { Filter } from './Filter/Filter';
 import css from 'App.module.css';
 
 export function App() {
+  const { isLoading, error } = useSelector(state => state.contacts);
+
+  const dispatch = useDispatch();
+  const contacts = useSelector(addContact);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (error) return alert(`${error} error`);
+  }, [error]);
+
   return (
     <section className={css.content}>
       <div className={css.content__container}>
         <ContactForm />
         <h2 className={css.Contacts}>Contacts</h2>
-        <Filter />
-        <ContactList />
+        {!isLoading && contacts.length > 0 && <Filter />}
+        {!isLoading && contacts.length > 0 && <ContactList />}
       </div>
     </section>
   );
